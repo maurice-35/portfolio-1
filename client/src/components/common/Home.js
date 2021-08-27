59345bd9b598aa5e9294798df0e4e3f19d3690c6
Mauricekollewe <kollewem.35@gmail.com>
2021-08-27 10:19:39 +0100

    update

--- client/src/components/common/Home.js
+++ client/src/components/common/Home.js
@@ -13,13 +13,19 @@ const Home = () => {
   return (
     <>
       <Navbar className="teal" expand="bs">
-        <Navbar.Brand className="icon" href="#">♻︎</Navbar.Brand>
-        <h4>The  love  for  cats!</h4>
+        <Navbar.Brand id="icon" href="#">♻︎</Navbar.Brand>
+        <div className="navbar-wrapper">
+          <h4>The  love  for  cats!</h4><br />
+          <p>In The Kindom Of Love, Every Life Counts</p>
+        </div>
         <Card className="pic">
           <img src="https://res.cloudinary.com/doe5zwesw/image/upload/v1626729161/cat-1_lg8pro.jpg" />
         </Card>
-        <Navbar.Brand href="#">♻︎</Navbar.Brand>
-        <h4>Kind  minds  never  hide!</h4>
+        <Navbar.Brand id="icon" href="#">♻︎</Navbar.Brand>
+        <div className="navbar-wrapper">
+          <h4>Kind  minds  never  hide!</h4><br />
+          <p>The Journal Of A Thousand Miles.</p>
+        </div>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
