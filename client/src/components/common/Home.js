4ca5b991794c4732454b8569cdade18b75d0daf1
Mauricekollewe <kollewem.35@gmail.com>
2021-09-15 07:21:18 +0100

    updates

--- client/src/components/common/Home.js
+++ client/src/components/common/Home.js
@@ -63,87 +63,109 @@ const Home = () => {
               </Col>
             </Row><br />
             <h3>Technologies</h3><br />
-            <Row className="ICONS">
-              <Col xs={5} md={3}>
+            <Row className="ICONS" id="font">
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630697355/html_rtvtmb.png" rounded />
                 <h3>HTML</h3>
               </Col>
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630679316/py3_z3bzw3.png" rounded />
                 <h3>Python</h3>
               </Col>
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630742605/bulma_xuw6t5.png" rounded />
                 <h3>Bulma</h3>
               </Col>
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_100/v1630714201/bstrap1_f7x2c1.png" rounded />
                 <h3>Bootstrap</h3>
               </Col>
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630697648/node3_fid6ns.png" rounded />
                 <h3>Node.js</h3>
               </Col>
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630744116/axios_ltorsb.png" rounded />
                 <h3>Axios</h3>
               </Col>
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630705675/css.png" rounded />
                 <h3>CSS</h3>
               </Col>
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630706380/sass1_tg8vcs.png" rounded />
                 <h3>Sass</h3>
               </Col>
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630705709/mongo_zyp23q.png" rounded />
                 <h3>MongoDB</h3>
               </Col>
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_80/v1630706859/exp_nt3jho.png" thumbnail />
                 <h3>Express</h3>
               </Col>
-              <Col xs={5} md={3}>
-                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_80/v1630708045/psql_nolbul.png" thumbnail />
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
+                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_70/v1630708045/psql_nolbul.png" thumbnail />
                 <h3>PosgreSQL</h3>
               </Col>
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_80/v1630706895/react_b0cycf.png" thumbnail />
                 <h3>React</h3>
               </Col>
-              <Col xs={5} md={3}>
-                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_80/v1631004003/jg3_q75wjm.png" thumbnail />
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
+                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_70/v1631004003/jg3_q75wjm.png" thumbnail />
                 <h3>Django</h3>
               </Col>
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_thumb,w_120,g_face/v1630743172/jwt_svaw6v.png" thumbnail />
-                <h3>Json Web Token</h3>
+                <h3>JSON Web Token</h3>
               </Col>
-              <Col xs={5} md={3}>
-                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_80/v1630708004/herok_xjwyiq.png" thumbnail />
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
+                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_70/v1630708004/herok_xjwyiq.png" thumbnail />
                 <h3>Heroku</h3>
               </Col>
-              <Col xs={5} md={3}>
-                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_80/v1630711706/gith_lszan7.png" thumbnail />
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
+                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_70/v1630711706/gith_lszan7.png" thumbnail />
                 <h3>GitHub</h3>
               </Col>
-              <Col xs={5} md={3}>
-                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_80/v1630747880/insom_w2m7xy.png" thumbnail />
-                <h3>Imsomnia</h3>
-              </Col><Col xs={5} md={3}>
-                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_80/v1630747998/Tplus_cyeuex.jpg" thumbnail />
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
+                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_70/v1630747880/insom_w2m7xy.png" thumbnail />
+                <h3>Insomnia</h3>
+              </Col>
+              {/* </Col><Col xs={5} md={3}> */}
+              <Col xs="auto">
+                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_70/v1630747998/Tplus_cyeuex.jpg" thumbnail />
                 <h3>TablePlus</h3>
               </Col>
-              <Col xs={5} md={3}>
-                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_80/v1630748656/trello1_gcntk6.png" thumbnail />
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
+                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_70/v1630748656/trello1_gcntk6.png" thumbnail />
                 <h3>Trello</h3>
               </Col>
-              <Col xs={5} md={3}>
-                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_80/v1630747638/figma_znwh7b.png" thumbnail />
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
+                <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_70/v1630747638/figma_znwh7b.png" thumbnail />
                 <h3>Figma</h3>
               </Col>
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_80/v1630748386/miro_vyaqjw.png" thumbnail />
                 <h3>Miro</h3>
               </Col>
@@ -214,7 +236,8 @@ const Home = () => {
             </Row>
 
             <Row className="tools" id="paint">
-              <Col xs={5} md={3}>
+              {/* <Col xs={5} md={3}> */}
+              <Col xs="auto">
                 <p className="describe">I enjoy painting during my spare time.</p>
                 <Image src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,w_700/v1631098527/paint_zxpgdl.jpg" /><span />
                 <p className="describe">I enjoy cycling around the town and my neigbourhood.</p>
