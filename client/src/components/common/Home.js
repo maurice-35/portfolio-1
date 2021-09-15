9ca93e0a9918ccb29e69a77b864f4536dcc7858b
Mauricekollewe <kollewem.35@gmail.com>
2021-09-15 08:37:05 +0100

    updates

--- client/src/components/common/Home.js
+++ client/src/components/common/Home.js
@@ -21,8 +21,7 @@ const Home = () => {
       <section data-spy="scroll" data-target=".navbar" data-offset="75">
         <div id="myName">
           <Container>
-            {/* <Col xs={16} md={12} id="myName"> */}
-            <Col xs="auto" id="myName">
+            <Col xs={16} md={12} id="myName">
               <div id="fa-fa">
                 <Card.Link><a href="mailto:kollewem.35@gmail.com"><i className="fa fa-at icon"></i></a></Card.Link>
               </div>
@@ -45,8 +44,7 @@ const Home = () => {
           <Container>
             <Row className="ICONS">
               <img id="profile" src="https://res.cloudinary.com/doe5zwesw/image/upload/v1631217534/profile_ijyamh.jpg" thumbnail width="150" />
-              {/* <Col xs={8} md={6}> */}
-              <Col xs="auto">
+              <Col xs={8} md={6}>
                 <p className="describe"> I am Maurice. A Software Engineer based in Greater London.</p>
                 <p className="describe">During my volunteering role behind the till at a charity shop, I was curious about the technology used to process receipts, and I became passionate about the impact of computing and technology on the modern world. After online searches, I developed more of an interest and started taking self learning online and writing some codes. I later enrolled in a full time Software Engineering course in order to acquire new skills and develop the passion and curiosity to solve problems. I thrive when building on my new skills and excel when collaborating and working as a team.</p>
                 <p className="describe">With my recent engineering and customer service skills, and a strong desire to learn, I am looking to progress in the tech industry as a Junior Software Engineer.</p>
@@ -60,115 +58,93 @@ const Home = () => {
           <h1 id="exp">Experience</h1><br /><span />
           <Container>
             <Row>
-              {/* <Col> */}
-              <Col xs="auto">
+              <Col>
                 <h3> <img src="https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png"></img><span />General Assembly</h3>
               </Col>
             </Row><br />
             <h3>Technologies</h3><br />
-            <Row className="ICONS" id="font">
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+            <Row className="ICONS">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630697355/html_rtvtmb.png" rounded />
                 <h3>HTML</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630679316/py3_z3bzw3.png" rounded />
                 <h3>Python</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630742605/bulma_xuw6t5.png" rounded />
                 <h3>Bulma</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_100/v1630714201/bstrap1_f7x2c1.png" rounded />
                 <h3>Bootstrap</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630697648/node3_fid6ns.png" rounded />
                 <h3>Node.js</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630744116/axios_ltorsb.png" rounded />
                 <h3>Axios</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630705675/css.png" rounded />
                 <h3>CSS</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630706380/sass1_tg8vcs.png" rounded />
                 <h3>Sass</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale/v1630705709/mongo_zyp23q.png" rounded />
                 <h3>MongoDB</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_80/v1630706859/exp_nt3jho.png" thumbnail />
                 <h3>Express</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_70/v1630708045/psql_nolbul.png" thumbnail />
                 <h3>PosgreSQL</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_80/v1630706895/react_b0cycf.png" thumbnail />
                 <h3>React</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_70/v1631004003/jg3_q75wjm.png" thumbnail />
                 <h3>Django</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_thumb,w_120,g_face/v1630743172/jwt_svaw6v.png" thumbnail />
                 <h3>JSON Web Token</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_70/v1630708004/herok_xjwyiq.png" thumbnail />
                 <h3>Heroku</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,e_grayscale,w_70/v1630711706/gith_lszan7.png" thumbnail />
                 <h3>GitHub</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_70/v1630747880/insom_w2m7xy.png" thumbnail />
                 <h3>Insomnia</h3>
               </Col>
-              {/* </Col><Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_70/v1630747998/Tplus_cyeuex.jpg" thumbnail />
                 <h3>TablePlus</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_70/v1630748656/trello1_gcntk6.png" thumbnail />
                 <h3>Trello</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_70/v1630747638/figma_znwh7b.png" thumbnail />
                 <h3>Figma</h3>
               </Col>
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              <Col xs={5} md={3}>
                 <Image className="boot" src="https://res.cloudinary.com/doe5zwesw/image/upload/e_grayscale,w_80/v1630748386/miro_vyaqjw.png" thumbnail />
                 <h3>Miro</h3>
               </Col>
@@ -239,8 +215,8 @@ const Home = () => {
             </Row>
 
             <Row className="tools" id="paint">
-              {/* <Col xs={5} md={3}> */}
-              <Col xs="auto">
+              {/* <Col xs="auto"> */}
+              <Col xs={5} md={3}>
                 <p className="describe">I enjoy painting during my spare time.</p>
                 <Image src="https://res.cloudinary.com/doe5zwesw/image/upload/c_scale,w_700/v1631098527/paint_zxpgdl.jpg" /><span />
                 <p className="describe">I enjoy cycling around the town and my neigbourhood.</p>
@@ -255,10 +231,8 @@ const Home = () => {
           <Card className="d-block w-100" id="INFO">
             <Container fluid="lg" className="mr-auto my-4 my-md-5">
               <Row className="justify-content-md-center">
-                {/* <Col> */}
-                <Col xs="auto">
+                <Col>
                   <Form onSubmit={handleSubmit} className='function'>
-
                     <Form.Group className="info">
                       <Form.Label>Name</Form.Label>
                       <Form.Control name="email" type="email" placeholder="Enter email" />
@@ -279,7 +253,6 @@ const Home = () => {
                         <Form.Control as="textarea" rows={3} />
                       </Form.Group>
                     </div>
-
                     <button type="submit" className="btn" id="butt"> <a href="mailto:kollewem.35@gmail.com">Send Message</a></button>
                   </Form>
                 </Col>
